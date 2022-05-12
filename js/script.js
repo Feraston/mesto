41e536234c2ecb937705eac852c3173ed29a08d8
Roman Prilyuk <feraston@yandex.ru>
2022-05-12 23:58:20 +0300

    fix: внести правки после ревью

--- js/script.js
+++ js/script.js
@@ -4,28 +4,27 @@ const closePopup = document.querySelector('.popup__close');
 const nameAvtor = document.querySelector('.profile__name');
 const postAvtor = document.querySelector('.profile__post')
 let formElement = document.querySelector('.popup__form-edit');
-let nameInput = document.querySelector('.popup__form-name');
-let jobInput = document.querySelector('.popup__form-post');
+let nameInput = document.querySelector('.popup__form-input:nth-of-type(1)');
+let jobInput = document.querySelector('.popup__form-input:nth-of-type(2)');
 
 function togglePopup() {
-  popup.classList.toggle('popup__open');
+  popup.classList.toggle('popup_open');
+  nameInput.value = nameAvtor.textContent;
+  jobInput.value = postAvtor.textContent;
 };
-
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
-
-popup.addEventListener('click', function(ev) {
-    if(ev.target === ev.currentTarget) {
-      popup.classList.toggle('popup__open');
-    }
-});
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-
   nameAvtor.textContent = nameInput.value;
   postAvtor.textContent = jobInput.value;
   togglePopup();
 };
 
+openPopup.addEventListener('click', togglePopup);
+closePopup.addEventListener('click', togglePopup);
+popup.addEventListener('click', function(ev) {
+  if(ev.target === ev.currentTarget) {
+    popup.classList.toggle('popup_open');
+  }
+});
 formElement.addEventListener('submit', formSubmitHandler); 
