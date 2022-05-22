275ebfe6273f41d037bbc7dc4a4735b2ffa73bd9
Roman Prilyuk <feraston@yandex.ru>
2022-05-23 01:40:32 +0300

    feat: добавить popup Добавить место

--- js/script.js
+++ js/script.js
@@ -1,34 +1,42 @@
-const openPopup = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
-const closePopup = document.querySelector('.popup__close');
+const openPopupEdit = document.querySelector('.profile__edit');
+const openPopupAddCard = document.querySelector('.profile__add');
+const popup = document.querySelectorAll('.popup');
+const closePopup = document.querySelectorAll('.popup__close');
 const nameAvtor = document.querySelector('.profile__name');
 const postAvtor = document.querySelector('.profile__post')
 let formElement = document.querySelector('.popup__form-edit');
-let nameInput = document.querySelector('.popup__form-input:nth-of-type(1)');
-let jobInput = document.querySelector('.popup__form-input:nth-of-type(2)');
+let nameInput = document.querySelectorAll('.popup__form-input:nth-of-type(1)');
+let jobInput = document.querySelectorAll('.popup__form-input:nth-of-type(2)');
 
-function togglePopup() {
-  if (popup.classList.contains('popup_open')){
-      popup.classList.toggle('popup_open');
+function popupEditProfile() {
+  if (popup[0].classList.contains('popup_open')){
+      popup[0].classList.toggle('popup_open');
   } else {
-    popup.classList.toggle('popup_open');
-    nameInput.value = nameAvtor.textContent;
-    jobInput.value = postAvtor.textContent;
+    popup[0].classList.toggle('popup_open');
+    nameInput[0].value = nameAvtor.textContent;
+    jobInput[0].value = postAvtor.textContent;
+  };
+};
+
+function popupAddCard() {
+  if (popup[1].classList.contains('popup_open')){
+      popup[1].classList.toggle('popup_open');
+  } else {
+    popup[1].classList.toggle('popup_open');
   };
 };
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-  nameAvtor.textContent = nameInput.value;
-  postAvtor.textContent = jobInput.value;
-  togglePopup();
+  nameAvtor.textContent = nameInput[0].value;
+  postAvtor.textContent = jobInput[0].value;
+  popupEditProfile();
 };
 
-openPopup.addEventListener('click', togglePopup);
-closePopup.addEventListener('click', togglePopup);
-popup.addEventListener('click', function(ev) {
-  if(ev.target === ev.currentTarget) {
-    popup.classList.toggle('popup_open');
-  }
-});
+openPopupEdit.addEventListener('click', popupEditProfile);
+closePopup[0].addEventListener('click', popupEditProfile);
+
+openPopupAddCard.addEventListener('click', popupAddCard);
+closePopup[1].addEventListener('click', popupAddCard);
+
 formElement.addEventListener('submit', formSubmitHandler);
