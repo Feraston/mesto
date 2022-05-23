d25822d3ea2cdae4c35e2fac88539a52fe1fbd63
Roman Prilyuk <feraston@yandex.ru>
2022-05-24 00:29:14 +0300

    feat: реализовать добавление новых карточек

--- js/script.js
+++ js/script.js
@@ -4,44 +4,42 @@ const popup = document.querySelectorAll('.popup');
 const closePopup = document.querySelectorAll('.popup__close');
 const nameAvtor = document.querySelector('.profile__name');
 const postAvtor = document.querySelector('.profile__post');
-let formElement = document.querySelector('.popup__form-edit');
-let nameInput = document.querySelectorAll('.popup__form-input:nth-of-type(1)');
-let jobInput = document.querySelectorAll('.popup__form-input:nth-of-type(2)');
-
-// Template
+const formEdit = document.querySelector('.popup__form-edit');
+const formAdd = document.querySelector('.popup__form-add');
+const nameInput = document.querySelectorAll('.popup__form-input:nth-of-type(1)');
+const jobLinkInput = document.querySelectorAll('.popup__form-input:nth-of-type(2)');
 const contentCards = document.querySelector('.content__cards');
 const contentTemplate = document.querySelector('#template__contents').content;
-const cardTemplate = contentTemplate.querySelector('.content__card').cloneNode(true);
-cardTemplate.querySelector('.content__scenery').src='./images/foto_one.jpg';
-cardTemplate.querySelector('.content__scenery').alt='Новинка';
-cardTemplate.querySelector('.content__place').textContent = 'Дюк Корморант';
-
-contentCards.append(cardTemplate); 
-
 const initialCards = [
   {
     name: 'Горная дорога',
-    link: 'images/foto_one.jpg'
+    link: 'images/foto_one.jpg',
+    alt: 'Горная дорога'
   },
   {
     name: 'Горный мир',
-    link: 'images/foto_two.jpg'
+    link: 'images/foto_two.jpg',
+    alt: 'Горный мир'
   },
   {
     name: 'Дом на краю света',
-    link: 'images/foto_three.jpg'
+    link: 'images/foto_three.jpg',
+    alt: 'Дом на краю света'
   },
   {
     name: 'Зимний лес',
-    link: 'images/foto_four.jpg'
+    link: 'images/foto_four.jpg',
+    alt: 'Зимний лес'
   },
   {
     name: 'Тропа к вечности',
-    link: 'images/foto_five.jpg'
+    link: 'images/foto_five.jpg',
+    alt: 'Тропа к вечности'
   },
   {
     name: 'Место силы',
-    link: 'images/foto_six.jpg'
+    link: 'images/foto_six.jpg',
+    alt: 'Место силы'
   }
 ]; 
 
@@ -51,29 +49,45 @@ function popupEditProfile() {
   } else {
     popup[0].classList.toggle('popup_open');
     nameInput[0].value = nameAvtor.textContent;
-    jobInput[0].value = postAvtor.textContent;
+    jobLinkInput[0].value = postAvtor.textContent;
   };
 };
 
 function popupAddCard() {
-  if (popup[1].classList.contains('popup_open')){
       popup[1].classList.toggle('popup_open');
-  } else {
-    popup[1].classList.toggle('popup_open');
-  };
 };
 
+function formAddElement(evt) {
+  evt.preventDefault();
+  const cardTemplate = contentTemplate.querySelector('.content__card').cloneNode(true);
+  cardTemplate.querySelector('.content__scenery').src= jobLinkInput[1].value;
+  cardTemplate.querySelector('.content__scenery').alt= nameInput[1].value;
+  cardTemplate.querySelector('.content__place').textContent = nameInput[1].value;
+  contentCards.prepend(cardTemplate);
+  nameInput[1].value = '';
+  jobLinkInput[1].value = '';
+  popupAddCard();
+};
+
+initialCards.forEach((element) => {
+  const cardTemplate = contentTemplate.querySelector('.content__card').cloneNode(true);
+  cardTemplate.querySelector('.content__scenery').src= element.link;
+  cardTemplate.querySelector('.content__scenery').alt= element.alt;
+  cardTemplate.querySelector('.content__place').textContent = element.name;
+  contentCards.append(cardTemplate); 
+});
+
 function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   nameAvtor.textContent = nameInput[0].value;
-  postAvtor.textContent = jobInput[0].value;
+  postAvtor.textContent = jobLinkInput[0].value;
   popupEditProfile();
 };
 
 openPopupEdit.addEventListener('click', popupEditProfile);
 closePopup[0].addEventListener('click', popupEditProfile);
-
 openPopupAddCard.addEventListener('click', popupAddCard);
 closePopup[1].addEventListener('click', popupAddCard);
 
-formElement.addEventListener('submit', formSubmitHandler);
+formEdit.addEventListener('submit', formSubmitHandler);
+formAdd.addEventListener('submit', formAddElement);
